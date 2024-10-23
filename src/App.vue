c8571d090e89e49de1ecf5dc12933bbe0529baeb
zach0429 <set48989@gmail.com>
2024-10-23 15:54:54 +0800

    set up

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Vite + Vue" />
+  <div class="bb">
+    <p>123</p>
+  </div>
+</template>
+
+<style lang="scss">
+  .bb {
+    height: 50px;
+    width: 50px;
+    background-color: blue;
+    p {
+      color: yellow;
+    }
+  }
+</style>
